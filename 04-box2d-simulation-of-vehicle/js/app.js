956275b9eb3b927941a1be90e48bd338a4f4ea52
Eugene Krevenets <hyzhak@ya.ru>
2013-04-14 01:37:42 +0200

    tune a little vehicle to small car

--- 04-box2d-simulation-of-vehicle/js/app.js
+++ 04-box2d-simulation-of-vehicle/js/app.js
@@ -23,9 +23,11 @@ world.$add('ng2DViewPort', {
 
 world.$add('ngBox2DSystem', {
     gravity: {
-        x:0,
-        y:9.8
-    }
+        x: 0,
+        y: 9.8
+    },
+    velocityIterations: 10,
+    positionIterations: 10
 });
 
 world.$add('ngBox2DDebugDraw', {
@@ -58,14 +60,10 @@ for (var i = 0, l = 50; i < l; i++) {
 //        'ngSpriteAtlas' : { name: 'box' + boxType + '.png', url: 'assets/spritesheet.json', fitToSize: true},
 //            'ngMovieClip' : {url: 'assets/explosion.json', fitToSize: true, frames: ['Explosion_Sequence_A 1.png', 'Explosion_Sequence_A 2.png', 'Explosion_Sequence_A 3.png', 'Explosion_Sequence_A 4.png', 'Explosion_Sequence_A 5.png', 'Explosion_Sequence_A 6.png', 'Explosion_Sequence_A 7.png', 'Explosion_Sequence_A 8.png', 'Explosion_Sequence_A 9.png', 'Explosion_Sequence_A 10.png', 'Explosion_Sequence_A 11.png', 'Explosion_Sequence_A 12.png', 'Explosion_Sequence_A 13.png', 'Explosion_Sequence_A 14.png', 'Explosion_Sequence_A 15.png', 'Explosion_Sequence_A 16.png', 'Explosion_Sequence_A 17.png', 'Explosion_Sequence_A 18.png', 'Explosion_Sequence_A 19.png', 'Explosion_Sequence_A 20.png', 'Explosion_Sequence_A 21.png', 'Explosion_Sequence_A 22.png', 'Explosion_Sequence_A 23.png', 'Explosion_Sequence_A 24.png', 'Explosion_Sequence_A 25.png', 'Explosion_Sequence_A 26.png', 'Explosion_Sequence_A 27.png']},
         'ng2D': {x : width / 2 + (width / 2) * Math.random(), y: 40 + (height - 40) * Math.random()},
-        'ng2DSize': {width:30, height:30},
+        'ng2DSize': {width:10, height:10},
         'ng2DRotation': {},
         'ngPhysic': {},
-        'ngCollisionGroup': {
-            'neverWith': 'stones'
-        },
-        'ngDraggable': {},
-        'ngFixedRotation': {}
+        'ngDraggable': {}
     }));
 //    (function(box) {
 //        setTimeout(function() {
@@ -76,31 +74,35 @@ for (var i = 0, l = 50; i < l; i++) {
 
 //vehicle
 
-function vehicle(x, y, name){
-    var carWidth = 45;
-    var carHeight = 10;
-    var axleContainerDistance = 30;
-    var axleContainerWidth = 5;
-    var axleContainerHeight = 20;
-    var axleContainerDepth = 10;
-    var axleAngle = 20;
-    var wheelRadius = 25;
+function vehicle(x, y, name, newOps){
+    newOps = newOps || {};
+    var ops = {
+        width: newOps.width || 45.0,
+        height: newOps.height || 10.0,
+        axleContainerDistance: newOps.axleContainerDistance || 30.0,
+        axleContainerWidth: newOps.axleContainerWidth ||  5.0,
+        axleContainerHeight: newOps.axleContainerHeight || 20.0,
+        axleContainerDepth: newOps.axleContainerDepth || 10.0,
+        axleAngle: newOps.axleAngle || 20.0,
+        wheelRadius: newOps.wheelRadius || 25.0,
+        wheelMaxSpeed: newOps.wheelMaxSpeed || 20.0,
+        wheelAcceleration: newOps.wheelAcceleration || 1.0,
+        rearWheelDrive: true,
+        frontWheelDrive: true
+    };
 
-    var wheelMaxSpeed = 10.0;
-    var wheelAcceleration = 0.5;
-
-    var degreesToRadians = Math.PI / 180;
+    var degreesToRadians = Math.PI / 180.0;
 
     //body
     var bodyName = 'vehicle-body-' + name;
     world.$add(world.$e(bodyName, {
         'ng2D': {x : x, y: y},
-        'ng2DSize': {width:2 * carWidth, height:2 * carHeight},
+        'ng2DSize': {width: 2.0 * ops.width, height: 2.0 * ops.height},
         'ng2DRotation': {},
         'ngDraggable': {},
         'ngPhysic': {
-            restitution: 0.0,
-            friction: 200.0,
+            restitution: 0.3,
+            friction: 3.0,
             density: 0.5
         },
         'ngCollisionGroup': {
@@ -111,14 +113,16 @@ function vehicle(x, y, name){
     //suspension
     //* left-container
     world.$add(world.$e('vehicle-left-suspension-container-' + name, {
-        'ng2D': {x : x - axleContainerDistance, y: y + axleContainerDepth},
-        'ng2DSize': {width: 2 * axleContainerWidth, height: 2 * axleContainerHeight},
-        'ng2DRotation': { rotation: axleAngle*degreesToRadians },
+        'ng2D': {
+            x: x - ops.axleContainerDistance,
+            y: y + ops.axleContainerDepth},
+        'ng2DSize': {width: 2 * ops.axleContainerWidth, height: 2 * ops.axleContainerHeight},
+        'ng2DRotation': { rotation: ops.axleAngle*degreesToRadians },
         'ngPhysic': {
             partOf: bodyName,
             restitution: 0.0,
             friction: 200.0,
-            density: 0.5
+            density: 1.0
         },
         'ngCollisionGroup': {
             'neverWith': 'vehicle'
@@ -127,20 +131,20 @@ function vehicle(x, y, name){
 
     //* left-axle
     var leftSuspensionAxleName = 'vehicle-left-suspension-axle-' + name;
-    var leftSuspensionAxleX = x - axleContainerDistance - axleContainerHeight * Math.cos((90 - axleAngle) * degreesToRadians);
-    var leftSuspensionAxleY = y + axleContainerDepth + axleContainerHeight * Math.sin((90 - axleAngle) * degreesToRadians);
+    var leftSuspensionAxleX = x - ops.axleContainerDistance - ops.axleContainerHeight * Math.cos((90 - ops.axleAngle) * degreesToRadians);
+    var leftSuspensionAxleY = y + ops.axleContainerDepth + ops.axleContainerHeight * Math.sin((90 - ops.axleAngle) * degreesToRadians);
 
     world.$add(world.$e(leftSuspensionAxleName, {
         'ng2D': {
             x: leftSuspensionAxleX,
             y: leftSuspensionAxleY
         },
-        'ng2DSize': {width: 2 * axleContainerWidth / 2, height: 2 * axleContainerHeight},
-        'ng2DRotation': { rotation: axleAngle*degreesToRadians },
+        'ng2DSize': {width: 2 + 2 * ops.axleContainerWidth / 2, height: 2 * ops.axleContainerHeight},
+        'ng2DRotation': { rotation: ops.axleAngle * degreesToRadians },
         'ngPhysic': {
             restitution: 0.0,
             friction: 200.0,
-            density: 0.5
+            density: 1.0
         },
         'ngCollisionGroup': {
             'neverWith': 'vehicle'
@@ -149,9 +153,9 @@ function vehicle(x, y, name){
 
     //* right-container
     world.$add(world.$e('vehicle-left-suspension-container-' + name, {
-        'ng2D': {x : x + axleContainerDistance, y: y + axleContainerDepth},
-        'ng2DSize': {width: 2 * axleContainerWidth, height: 2 * axleContainerHeight},
-        'ng2DRotation': { rotation: -axleAngle*degreesToRadians },
+        'ng2D': {x : x + ops.axleContainerDistance, y: y + ops.axleContainerDepth},
+        'ng2DSize': {width: 2 * ops.axleContainerWidth, height: 2 * ops.axleContainerHeight},
+        'ng2DRotation': { rotation: -ops.axleAngle * degreesToRadians },
         'ngPhysic': {
             partOf: bodyName,
             restitution: 0.0,
@@ -165,16 +169,16 @@ function vehicle(x, y, name){
 
     //* right-axle
     var rightSuspensionAxleName = 'vehicle-right-suspension-axle-' + name;
-    var rightSuspensionAxleX = x + axleContainerDistance + axleContainerHeight * Math.cos((90 - axleAngle) * degreesToRadians);
-    var rightSuspensionAxleY = y + axleContainerDepth + axleContainerHeight * Math.sin((90 - axleAngle) * degreesToRadians);
+    var rightSuspensionAxleX = x + ops.axleContainerDistance + ops.axleContainerHeight * Math.cos((90 - ops.axleAngle) * degreesToRadians);
+    var rightSuspensionAxleY = y + ops.axleContainerDepth + ops.axleContainerHeight * Math.sin((90 - ops.axleAngle) * degreesToRadians);
 
     world.$add(world.$e(rightSuspensionAxleName, {
         'ng2D': {
             x: rightSuspensionAxleX,
             y: rightSuspensionAxleY
         },
-        'ng2DSize': {width: 2 * axleContainerWidth / 2, height: 2 * axleContainerHeight},
-        'ng2DRotation': { rotation: -axleAngle*degreesToRadians },
+        'ng2DSize': {width: 2 + 2 * ops.axleContainerWidth / 2, height: 2 * ops.axleContainerHeight},
+        'ng2DRotation': { rotation: -ops.axleAngle * degreesToRadians },
         'ngPhysic': {
             restitution: 0.0,
             friction: 200.0,
@@ -190,17 +194,17 @@ function vehicle(x, y, name){
     var leftWheelName = 'vehicle-left-wheel-' + name;
     world.$add(world.$e(leftWheelName, {
         'ng2D': {
-            x: x - axleContainerDistance - 2 * axleContainerHeight * Math.cos((90 - axleAngle) * degreesToRadians),
-            y: y + axleContainerDepth + 2 * axleContainerHeight * Math.sin((90 - axleAngle) * degreesToRadians)
+            x: x - ops.axleContainerDistance - 2 * ops.axleContainerHeight * Math.cos((90 - ops.axleAngle) * degreesToRadians),
+            y: y + ops.axleContainerDepth + 2 * ops.axleContainerHeight * Math.sin((90 - ops.axleAngle) * degreesToRadians)
         },
-        'ng2DCircle': {radius: wheelRadius},
+        'ng2DCircle': {radius: ops.wheelRadius},
         //'ng2DRotation',
         'ngSelected': {},
         'ngDraggable': {},
         'ngPhysic': {
-            restitution: 0.0,
-            friction: 200.0,
-            density: 0.5
+            restitution: 0.2,
+            friction: 15.0,
+            density: 1.0
         },
         'ngCollisionGroup': {
             'neverWith': 'vehicle'
@@ -211,18 +215,18 @@ function vehicle(x, y, name){
     var rightWheelName = 'vehicle-right-wheel-' + name;
     world.$add(world.$e(rightWheelName, {
         'ng2D': {
-            x: x + axleContainerDistance + 2 * axleContainerHeight * Math.cos((90 - axleAngle) * degreesToRadians),
-            y: y + axleContainerDepth + 2 * axleContainerHeight * Math.sin((90 - axleAngle) * degreesToRadians)
+            x: x + ops.axleContainerDistance + 2 * ops.axleContainerHeight * Math.cos((90 - ops.axleAngle) * degreesToRadians),
+            y: y + ops.axleContainerDepth + 2 * ops.axleContainerHeight * Math.sin((90 - ops.axleAngle) * degreesToRadians)
         },
-        'ng2DCircle': {radius: wheelRadius},
+        'ng2DCircle': {radius: ops.wheelRadius},
         //'ng2DRotation',
         'ngSelected': {},
         'ngDraggable': {},
         'ngPhysic': {
             name: rightWheelName,
-            restitution: 0.0,
-            friction: 200.0,
-            density: 0.5
+            restitution: 0.2,
+            friction: 15.0,
+            density: 1.5
         },
         'ngCollisionGroup': {
             'neverWith': 'vehicle'
@@ -233,39 +237,48 @@ function vehicle(x, y, name){
     //*left
     world.$add(world.$e('vehicle-left-wheel-revolute-joint-' + name, {
         'ng2D': {
-            x: x - axleContainerDistance - 2 * axleContainerHeight * Math.cos((90 - axleAngle) * degreesToRadians),
-            y: y + axleContainerDepth + 2 * axleContainerHeight * Math.sin((90 - axleAngle) * degreesToRadians)
+            x: x - ops.axleContainerDistance - 2 * ops.axleContainerHeight * Math.cos((90 - ops.axleAngle) * degreesToRadians),
+            y: y + ops.axleContainerDepth + 2 * ops.axleContainerHeight * Math.sin((90 - ops.axleAngle) * degreesToRadians)
         },
         'ngRevoluteJoint': {
+            //maxMotorTorque: 20.0,
+            bodyA: leftWheelName,
+            bodyB: leftSuspensionAxleName
         },
-        'ngEnableMotorOnKeyDown': {
+        'ngEnableMotorOnKeyDown': ops.rearWheelDrive?{
             keyCode: [37, 65],
             keyCodeReverse: [39, 68]
-        },
+        }:{},
         'ngMotorWithAcceleration': {
-            min:-wheelMaxSpeed,
-            max: wheelMaxSpeed,
-            acceleration: wheelAcceleration
+            min:-ops.wheelMaxSpeed,
+            max: ops.wheelMaxSpeed,
+            acceleration: ops.wheelAcceleration
         }
     }));
 
     //*right
     world.$add(world.$e('vehicle-right-wheel-revolute-joint-' + name, {
         'ng2D': {
-            x: x + axleContainerDistance + 2 * axleContainerHeight * Math.cos((90 - axleAngle) * degreesToRadians),
-            y: y + axleContainerDepth + 2 * axleContainerHeight * Math.sin((90 - axleAngle) * degreesToRadians)
+            x: x + ops.axleContainerDistance + 2 * ops.axleContainerHeight * Math.cos((90 - ops.axleAngle) * degreesToRadians),
+            y: y + ops.axleContainerDepth + 2 * ops.axleContainerHeight * Math.sin((90 - ops.axleAngle) * degreesToRadians)
         },
         'ngRevoluteJoint': {
-        }
-        /*,
-        'ngEnableMotorOnKeyDown': {
+            //maxMotorTorque: 20.0,
+            bodyA: rightWheelName,
+            bodyB: rightSuspensionAxleName
+        },
+        'ngEnableMotorOnKeyDown': ops.frontWheelDrive?{
             keyCode: [37, 65],
             keyCodeReverse: [39, 68]
+        }:{},
+        'ngMotorWithAcceleration': {
+            min:-ops.wheelMaxSpeed,
+            max: ops.wheelMaxSpeed,
+            acceleration: ops.wheelAcceleration
         }
-        */
     }));
 
-    //primatic-joints
+    //prismatic-joints
     //*left
     world.$add(world.$e('vehicle-left-wheel-prismatic-joint-' + name, {
         'ngPrismaticJoint': {
@@ -274,15 +287,15 @@ function vehicle(x, y, name){
                 y: leftSuspensionAxleY
             },
             anchorB: {
-                x: leftSuspensionAxleX + axleContainerDepth * Math.cos((90-axleAngle)*degreesToRadians),
-                y: leftSuspensionAxleY - axleContainerDepth * Math.sin((90-axleAngle)*degreesToRadians)
+                x: leftSuspensionAxleX + ops.axleContainerDepth * Math.cos((90 - ops.axleAngle)*degreesToRadians),
+                y: leftSuspensionAxleY - ops.axleContainerDepth * Math.sin((90 - ops.axleAngle)*degreesToRadians)
             },
             bodyA: leftSuspensionAxleName,
             bodyB: bodyName,
             maxMotorForce: 10.0,
             enableLimit: true,
             enableMotor: true,
-            motorSpeed: 10
+            motorSpeed: 10.0
         }
     }));
 
@@ -295,20 +308,24 @@ function vehicle(x, y, name){
                 y: rightSuspensionAxleY
             },
             anchorB: {
-                x: rightSuspensionAxleX - axleContainerDepth * Math.cos((90-axleAngle)*degreesToRadians),
-                y: rightSuspensionAxleY - axleContainerDepth * Math.sin((90-axleAngle)*degreesToRadians)
+                x: rightSuspensionAxleX - ops.axleContainerDepth * Math.cos((90 - ops.axleAngle) * degreesToRadians),
+                y: rightSuspensionAxleY - ops.axleContainerDepth * Math.sin((90 - ops.axleAngle) * degreesToRadians)
             },
             bodyA: rightSuspensionAxleName,
             bodyB: bodyName,
             maxMotorForce: 10.0,
             enableLimit: true,
             enableMotor: true,
-            motorSpeed: 10
+            motorSpeed: 10.0
         }
     }));
 }
 
-vehicle(100, 100, 'cabriolet');
+vehicle(100, 200, 'cabriolet', {
+    axleContainerHeight: 5,
+    axleContainerDepth: 2.5,
+    wheelRadius: 12
+});
 
 world.$add(world.$e('ground', [
     'ng2D', {x: width / 2, y: height},
