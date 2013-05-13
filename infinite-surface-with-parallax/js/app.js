bc5a1f5cf35ade00a7357e32a4b71eda386ff0a2
Eugene Krevenets <hyzhak@ya.ru>
2013-05-13 18:32:04 +0200

    add some comments

--- infinite-surface-with-parallax/js/app.js
+++ infinite-surface-with-parallax/js/app.js
@@ -6,8 +6,10 @@
 (function(darlingjs, darlingutil) {
     'use strict';
     var width = 640,
-        height = 480,
-        world = darlingjs.w('theCyclicWorld', [
+        height = 480;
+
+    //Create game world
+    var world = darlingjs.w('theCyclicWorld', [
         'ngFlatland',
         'ngCyclic',
         'ng3D',
@@ -16,10 +18,21 @@
         'ngPerformance'
     ]);
 
+    // ---------------------------------------------------------------
+    //
+    //  Systems adding section
+    //
+    // ---------------------------------------------------------------
+
+    //any system add some abilities
+
+    //start log performance
     world.$add('ngPerformanceStartLog');
 
+    //start log statistics
     world.$add('ngStatsBegin');
 
+    //define viewport properties
     var ng2DViewPort = world.$add('ng2DViewPort', {
         lookAt: {
             x: width / 2, y: height / 2
@@ -28,29 +41,42 @@
         height: height
     });
 
+    //visualization
     world.$add('ngPixijsStage', {
         domId: 'gameView',
-        width: width,// + 100,
-        height: height,// + 100,
+        width: width,
+        height: height,
         useWebGL: true
     });
 
-    world.$add('ngPixijsUpdateCycle');
-    world.$add('ngPixijsViewPortUpdateCycle');
+    //create sprites
     world.$add('ngPixijsSpriteFactory');
 
+    //update positions
+    world.$add('ngPixijsUpdateCycle');
+
+    //update position based on viewport
+    world.$add('ngPixijsViewPortUpdateCycle');
+
+    //convert ng3D properties to ngParallax.basis
     world.$add('ngConvert3DtoParallax');
+
+    //convert ngParallax and ng3D to ng2D
     world.$add('ngSimpleParallax');
 
+    //support cyclic of ng3D position (use ngCyclic)
     world.$add('ngMarkIfOutsideOfTheViewPortVertical3D');
     world.$add('ngMarkIfOutsideOfTheViewPortHorizontal3D');
     world.$add('ngMarkIfInsideOfTheViewPortVertical3D');
     world.$add('ngMarkIfInsideOfTheViewPortHorizontal3D');
     world.$add('ng3DCyclicLayer');
 
+    //defined custom system for rambling viewport
     var time = 0,
         speed = 2;
-    world.$s('', {
+
+    world.$s('ramblingViewport', {
+        //every frame we just update position of viewport
         $update: ['ng2DViewPort', function(ng2DViewPort) {
             ng2DViewPort.lookAt.x += speed * Math.cos(0.0007 * time);
             ng2DViewPort.lookAt.y += speed * Math.sin(0.0003 * time);
@@ -58,11 +84,21 @@
         }]
     });
 
+    //end of collecting statistics
     world.$add('ngStatsEnd', {
         domId: 'gameView'
     });
 
+    //end of collecting performance
     world.$add('ngPerformanceStopLog');
+
+    // ---------------------------------------------------------------
+    //
+    //  Entities adding section
+    //
+    // ---------------------------------------------------------------
+
+    // just feel square with entities
 
     var xStep = 16,
         yStep = 16,
@@ -74,6 +110,7 @@
         for (var i = 0; i < icount; i++) {
             var depth = Math.cos( 2 * Math.PI * j / jcount) * Math.cos( 2 * Math.PI * i / icount);
             world.$e('node_' + i + "_" + j, {
+
                 ng3D: {
                     x: xStep * i,
                     y: yStep * j,
@@ -87,7 +124,6 @@
                     patternHeight: yStep * jcount
                 },
 
-                ngPixijsSprite: false,
                 ngSprite : {
                     name: pixels[Math.floor(i) % 4],
                     spriteSheetUrl: 'assets/spritesheet.json',
@@ -100,6 +136,7 @@
         }
     }
 
-    world.$start();
+    //run world
 
+    world.$start();
 })(darlingjs, darlingutil);
